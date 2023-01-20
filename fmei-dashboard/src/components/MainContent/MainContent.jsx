015bf0a1e0dfb6678d6df8ba54bf5a9178cea115
Cristiano da Silva Monteiro <cristianodasilvamonteiro@gmail.com>
2023-01-19 22:22:12 -0300

    Creation of Mobile First- January 19, 2023

--- fmei-dashboard/src/components/MainContent/MainContent.jsx
+++ fmei-dashboard/src/components/MainContent/MainContent.jsx
@@ -1,15 +1,19 @@
 import styles from './MainContent.module.css';
 
+import statsIcon from '../../assets/icons/query_stats.svg';
+
 function MainContent(){
     return(
         <div className={styles.mainContent}>
-            <section>
-                <div className={styles.chartTitle}>
-                    <span></span>
+            <section className={styles.chartContainer}>
+                <div className={styles.text}>
+                    <img src={statsIcon} alt="Ícone" />
                     <h1>Informações</h1>
                 </div>
-                <div className={styles.chartContainer}>
-                    <div></div>
+                <div className={styles.charts}>
+                    <div className={styles.chart}></div>
+                    <div className={styles.chart}></div>
+                    <div className={styles.chart}></div>
                 </div>
             </section>
         </div>
