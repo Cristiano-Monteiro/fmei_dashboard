303f0a6565d3b808a58ce6859361f9179743f186
Cristiano da Silva Monteiro <91402144+Cristiano-Monteiro@users.noreply.github.com>
2023-01-27 11:37:09 -0300

    Update vite.config.js

--- fmei-dashboard/vite.config.js
+++ fmei-dashboard/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fmei_dashboard_react_js',
+  base: '/fmei-dashboard',
   plugins: [react()],
 })
